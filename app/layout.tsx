0d89443d171ba220c4e58ebd00e9a028242681fe
E.lev <e.lev@easysend.io>
2023-08-04 11:19:50 +0300

    remove theme-provider comp from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
-import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,9 +18,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <body className={inter.className}>{children}</body>
-        </ThemeProvider>
       </html>
     </ClerkProvider>
   );
